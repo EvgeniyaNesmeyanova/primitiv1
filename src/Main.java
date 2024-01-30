f2981c1397df49dcbed13b83a21b0bd148942a2d
Evgeniya Nes <prof_nea@mail.ru>
2024-01-30 17:09:22 +0500

    1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+        int amount=13676;
+        int mil=20;
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+        int bonus=amount/mil;
+        System.out.println("Вам начислено " + bonus + " бонус(ов)");
+    }
+}
